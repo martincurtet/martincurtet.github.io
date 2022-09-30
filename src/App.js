6f923a09797258cf9867e3e355eb7498f88d09bb
bep3 <martin.curtet@gmail.com>
2022-09-30 16:34:18 -0400

    description
    - added a small description
    - added github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Martin Curtet</h1>
+
+      <p>Hi, I'm a Fullstack Web Developer living in New York!</p>
+
+      <p>This online portfolio is under contruction, using React and hosted on Github Pages.</p>
+
+      <a href='https://github.com/martincurtet' target='_blank'>Github</a>
     </div>
   )
 }
